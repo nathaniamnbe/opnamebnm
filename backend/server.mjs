4427487d575db05f3719a5dbcc742e40fc023c55
nathaniamnbe <buildingprocess25@gmail.com>
2025-10-09 22:03:44 +0700

    nama kontraktor 5

--- backend/server.mjs
+++ backend/server.mjs
@@ -1106,6 +1106,7 @@ app.get("/api/opname/final", async (req, res) => {
       display_kontraktor:
         (row.get("kontraktor_username") || "").toString().trim() ||
         (row.get("kontraktor") || "").toString().trim(),
+        
     }));
 
     return res.status(200).json(submissions);
